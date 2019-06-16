2a2d7ad019f71f810ea302100e428b6f9ebdf51d
yonna <yonna@localhost.localdomain>
2019-06-17 00:06:54 +0200

    add cli

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/url"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"golang.org/x/crypto/acme/autocert"
 )
+
+var (
+	dns     string
+	appPort string
+)
+
+func init() {
+	flag.StringVar(&dns, "dns", "", "white hosted dns available for this app")
+	flag.StringVar(&appPort, "port", "", "port of running app")
+
+	flag.Parse()
+	if dns == "" {
+		log.Fatal("you need to set a dns we will use for the validation of https protocol")
+	}
+	if appPort == "" {
+		log.Fatal("Port of the reunning app to proxy")
+	}
+}
 
 func main() {
 	e := echo.New()
@@ -16,7 +36,7 @@ func main() {
 	e.Use(middleware.Secure())
 	e.Use(middleware.Gzip())
 
-	url1, err := url.Parse("http://127.0.0.1:8080/")
+	url1, err := url.Parse("http://127.0.0.1:" + appPort)
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
@@ -28,7 +48,7 @@ func main() {
 		})
 	e.Use(middleware.Proxy(b))
 	// dns autorisation
-	e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("api.yoannfort.ga")
+	e.AutoTLSManager.HostPolicy = autocert.HostWhitelist(dns)
 	// cache file
 	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 
